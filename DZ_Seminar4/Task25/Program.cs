dbb240b3dc4026be974fa147dab0f60f397d5951
ArtyomDey <a.dey@mail.ru>
2022-08-18 11:14:39 +0300

    Добавил Дз по С# 4 семинар

---
+++ DZ_Seminar4/Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿//=================Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введи число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введи степень для первого числа: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(a,b));
